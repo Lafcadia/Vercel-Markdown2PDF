12240f039008e0600739af9951b0f885328b5f2c
ChuishenX <chuishenx@oblivionocean.top>
2024-03-30 22:24:26 +0800

    First Attempt

--- api/index.py
+++ api/index.py
@@ -1,11 +1,44 @@
-from flask import Flask
+import os
+from flask import Flask, request, send_file
+import markdown
+from xhtml2pdf import pisa
+import io
+from xhtml2pdf.files import pisaFileObject
 
 app = Flask(__name__)
+a = os.getcwd()
+style = """<style type="text/css">
+    @font-face {
+        font-family: "Deng";
+        src: url('static/fonts/Deng.ttf');
+    }
+    p {font-family: Deng; word-wrap: break-word; word-break: normal; white-space: pre-wrap; -pdf-word-wrap: CJK;}
+    </style>"""
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def convert_page():
+    with open('index.html') as f:
+        html = f.read()
+    return html
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/upload',methods=['POST','GET'])
+def upload():
+    fila = io.BytesIO()
+    f = request.files['file'].read()
+    fp = str(f, encoding="utf-8")
+    html = markdown.markdown(fp)+style
+    # print(html)
+    pisaFileObject.getNamedFile = lambda self: self.uri
+    pisa_stats = pisa.CreatePDF(html, dest=fila, encoding='utf-8')
+    # print(fila.getvalue())
+    # print(pisa_stats.err)
+    pisa.showLogging()
+    # with open("a.html","wt", encoding="utf-8") as g:
+    #     g.write(html)
+    # with open("a.pdf","wb") as g:
+    #     g.write(fila.getvalue())
+    fila.seek(0)
+    return send_file(fila, mimetype='application/pdf')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
